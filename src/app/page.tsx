70252f6ac1c3b65e3b936d257ca3cacf7a2abc7f
Ali-zaid <az7425328@gmail.com>
2024-11-28 02:03:16 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,12 @@
-import 
+export default function Home() {
+  return (
+    <div className="main">
+      <div className="header grid-item">Header</div>
+      <div className="sidebar grid-item">Sidebar</div>
+      <div className="content1 grid-item">Content 1</div>
+      <div className="content2 grid-item">Content 2</div>
+      <div className="content3 grid-item">Content 3</div>
+      <div className="footer grid-item">Footer</div>
+    </div>
+  )
+}
